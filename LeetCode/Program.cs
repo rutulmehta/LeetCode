6eb31fc17f9c649c908bc0ca89e71ffa131f38db
rutulmehta <rutulmehta.job@gmail.com>
2025-05-24 18:54:22 -0400

    Remove Hello World from git

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,6 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using LeetCode;
-
-Console.WriteLine("Hello, World!");
 var returnValue = LeetCode_242.IsAnagram("anagram", "nagaram");
 Console.WriteLine(returnValue);
