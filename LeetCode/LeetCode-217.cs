fe22639c343003ffe8b618c2e6be9894ad092d47
rutulmehta2211 <rmehta29@hawk.iit.edu>
2025-05-24 12:10:31 -0400

    LeetCode-217

---
+++ LeetCode/LeetCode-217.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class LeetCode_217
+    {
+        public static bool main()
+        {
+            int[] numbers = { 1, 2, 4, 1, 9, 3, 6, 7 };
+            HashSet<int> set = new HashSet<int>(numbers);
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (set.Contains(numbers[i]))
+                {
+                    return true;
+                }
+                set.Add(numbers[i]);
+            }
+            return false;
+        }
+    }
+}
---
+++ LeetCode/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LeetCode;
+
+Console.WriteLine("Hello, World!");
+var returnValue = LeetCode_217.main();
+Console.WriteLine(returnValue);
